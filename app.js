1ba6a5d54050a0a7c45be98e7e4593749003faed
Jesse Shaw <jdshaw1987@gmail.com>
2018-01-02 14:55:10 -0500

    Updated App Page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +12,12 @@ app.get('/api/*', (req, res) => {
 app.get('/', (req, res) => {
   res.json({ok: true});
 });
+
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
