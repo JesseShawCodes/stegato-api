eaddc21fd80fb7e416830930a2876f2727bf68b8
Jesse Shaw <jdshaw1987@gmail.com>
2018-01-17 19:28:56 -0500

    Set up Auth System

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
+'use strict';
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const cors = require('cors');
-const {CLIENT_ORIGIN} = require('./config');
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
 const mongoose = require('mongoose');
-const fetch = require('node-fetch');
 const morgan = require('morgan');
 const passport = require('passport');
-
-// Logging
-app.use(morgan('common'));
-
-//Model
-var MusicInput = require('./models/music')
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-var router = express.Router();
-
-//MongoDb
-mongoose.connect('mongodb://localhost:27017/music');
 
 // Here we use destructuring assignment with renaming so the two variables
 // called router (from ./users and ./auth) have different names
@@ -31,6 +14,26 @@ mongoose.connect('mongodb://localhost:27017/music');
 // console.log(bobby); // De Niro - the variable name is bobby, not robert
 const { router: usersRouter } = require('./users');
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+
+const app = express();
+
+// Logging
+app.use(morgan('common'));
+
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
@@ -47,58 +50,9 @@ app.get('/api/protected', jwtAuth, (req, res) => {
   });
 });
 
-
-app.get('/api/*', (req, res) => {
-  res.json({ok: true});
-  console.log("Get request made to /api/*")
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
 });
-
-app.get('/', (req, res) => {
-  res.json({ok: true});
-  console.log("Get request made to /")
-});
-
-app.post('/rating/:id', (req, res) => {
-  var submission = new MusicInput();
-  submission.artist = req.headers.artist;
-  submission.album = req.headers.album;
-  submission.rating = req.headers.rating;
-  submission.identification = req.params.id;
-  console.log(req.params.id);
-  MusicInput.create(submission, function(err, submission) {
-    if(err) {
-      console.log(err);
-      console.log(`Error creating Data point: ${err[0]}`);
-      return err;
-    }
-    else {
-      console.log("Adding submission");
-      res.status(201).send();
-    }
-  })
-});
-
-app.use(
-  cors({
-      origin: CLIENT_ORIGIN
-  })
-);
-
-app.use(cors());
-
-// CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});
-
-
-app.use('/api', router);
 
 // Referenced by both runServer and closeServer. closeServer
 // assumes runServer has run and set `server` to a server object
@@ -106,7 +60,7 @@ let server;
 
 function runServer() {
   return new Promise((resolve, reject) => {
-    mongoose.connect(DATABASE_URL, { useMongoClient: true }, err => {
+    mongoose.connect(DATABASE_URL, err => {
       if (err) {
         return reject(err);
       }
@@ -142,10 +96,3 @@ if (require.main === module) {
 }
 
 module.exports = { app, runServer, closeServer };
-
-
-const PORT = 8080
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-module.exports = {app};
