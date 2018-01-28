630778c0d578a27dcef8fdce38d053e84e9d9fa3
Jesse Shaw <jdshaw1987@gmail.com>
2018-01-28 16:07:44 -0500

    Updated Get Requests for albums rated by a specific user

--- music-data/router.js
+++ music-data/router.js
@@ -13,24 +13,43 @@ const jsonParser = bodyParser.json();
 var { MusicInput } = require('./models')
 
 router.post('/:id', jsonParser, (req, res) => {
-    console.log(req.body);
     res.json({
         "We": "received it"
     })
     var submission = new MusicInput();
+    console.log(req.body)
     submission.artist = req.body.Artist;
     submission.album = req.body.album;
     submission.artwork = req.body.artwork;
-    submission.genre = req.body.genre;
-    submission.itunesLink = req.body.buyonitunes;
+    submission.genre = req.body.Genre;
+    submission.itunesLink = req.body.BuyOnItunes;
     submission.rating = req.body.Rating;
     submission.user = req.params.id;
     submission.collectionId = req.body.collectionid;
     console.log(submission);
     MusicInput.create(submission, function(err, submission) {
       console.log("Adding submission");
-      console.log(err);
+      // console.log(err);
     })
 });
+
+router.get('/:id', jsonParser, (req, res) => {
+    console.log(req.params.id);
+    let ret = [];
+    let rej = [];
+    MusicInput
+        .find()
+        .then(post => {
+            for (var i = 0; i < post.length; i++) {
+                if (req.params.id === post[i].user) {
+                    console.log(post[i]);
+                    ret.push(post[i]);
+                }
+                else rej.push(post[i]);
+            }
+            console.log(ret);
+            res.send(ret);
+        })
+})
 
 module.exports = {router};
